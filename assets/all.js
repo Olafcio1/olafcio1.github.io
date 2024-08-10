2482973105f5db31d534f6813276a4ae12c1ab66
Olafcio <olafcio@int.pl>
2024-08-10 11:32:59 +0200

    scroll fix

--- assets/all.js
+++ assets/all.js
@@ -2,9 +2,25 @@ addEventListener("load", _ => {
     document.body.classList.add("loaded");
 
     let ysliders = document.querySelectorAll(".yslider");
+    let ye = {};
+    let yi = {};
     setInterval(() => {
-        ysliders.forEach(el => {
-            el.scrollBy({ y: 2 });
+        ysliders.forEach((el, i) => {
+            if (!ye[i])
+                ye[i] = [...el.querySelectorAll("p")];
+            if (typeof yi[i] === 'undefined')
+                return yi[i] = 0;
+            let ei = yi[i]++;
+            if (ei == ye[i].length-2) {
+                yi[i] = 0;
+                ye[i].forEach(e => e.getAnimations().forEach(an => an.reverse() && setTimeout(() => an.cancel(), 500)));
+            } else {
+                let animopts = {duration:500,fill:"forwards",direction:"alternate",easing:"ease"};
+                ye[i][ei].animate([
+                    {scale:"1 1"},
+                    {scale:"1 0",marginTop:"-30px"}
+                ], animopts);
+            }
         });
-    }, 50);
+    }, 650);
 }, { once: true });
