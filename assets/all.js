e36321f42aac188a7cac571f15c04de78649b16e
Olafcio <olafcio@int.pl>
2024-08-10 21:14:41 +0200

    notif fix + flex fix for larger screens

--- assets/all.js
+++ assets/all.js
@@ -1,4 +1,24 @@
 addEventListener("load", _ => {
+    Object.defineProperty(window, 'notify', {value:function(msg) {
+        let el = document.createElement("div");
+        el.className = "notif";
+        el.innerHTML = `<i class="fas fa-checkmark"></i><p></p>`;
+        el.querySelector("p").innerText = msg;
+        document.body.append(el);
+        async function remove() {
+            el.animate([
+                {marginTop:0,scale:"1 1",opacity:1},
+                {marginTop:"-100%",scale:"1 0",opacity:0}
+            ], {duration: 400, direction: "alternate", easing: "ease", fill: "forwards"});
+            await new Promise(rsv => el.addEventListener("animationend", rsv, {once: true}));
+            el.remove();
+        }
+        let timeout = setTimeout(remove, 2000);
+        el.addEventListener("click", () => {
+            clearTimeout(timeout);
+            remove();
+        }, {once: true});
+    },writable:false});
     document.body.classList.add("loaded");
 
     let ysliders = document.querySelectorAll(".yslider");
