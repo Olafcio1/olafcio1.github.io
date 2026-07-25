640378cc9d08985461fc41d7f0671594b02b3a8e
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-25 17:57:13 +0200

    AntiAI section, footer

--- scripts/scrollcss.js
+++ scripts/scrollcss.js
@@ -1,3 +1,9 @@
 document.addEventListener("scroll", () => {
 	document.documentElement.style.setProperty("--scrollY", document.documentElement.scrollTop);
+	document.querySelectorAll(".animable").forEach(el => {
+		el.classList.toggle("visible", !(((
+			el.getBoundingClientRect().y +
+			el.getBoundingClientRect().height/3
+		)) >= document.documentElement.scrollTop));
+	});
 });
